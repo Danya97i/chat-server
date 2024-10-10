ad7891f2ad40e5337e0eb6a4fdf90900018c496e
Danya97i <dan.deg97@vk.com>
2024-10-10 12:40:21 +0300

    review fixes

--- cmd/grpc_server/server.go
+++ cmd/grpc_server/server.go
@@ -4,24 +4,28 @@ import (
 	"context"
 	"log"
 
-	pb "github.com/Danya97i/chat-server/pkg/chat_v1"
 	"google.golang.org/protobuf/types/known/emptypb"
+
+	pb "github.com/Danya97i/chat-server/pkg/chat_v1"
 )
 
 type server struct {
 	pb.UnimplementedChatV1Server
 }
 
-func (s *server) Create(_ context.Context, req *pb.CreateRequest) (*pb.CreateResponse, error) {
+// CreateChat - метод для создания нового чата
+func (s *server) CreateChat(_ context.Context, req *pb.CreateChatRequest) (*pb.CreateChatResponse, error) {
 	log.Println("create chat request", req)
 	return nil, nil
 }
 
-func (s *server) Delete(_ context.Context, req *pb.DeleteRequest) (*emptypb.Empty, error) {
+// DeleteChat - метод для удаления чата
+func (s *server) Delete(_ context.Context, req *pb.DeleteChatRequest) (*emptypb.Empty, error) {
 	log.Println("create chat request", req)
 	return nil, nil
 }
 
+// Метод для отправки сообщения в чат
 func (s *server) SendMesage(_ context.Context, req *pb.SendMessageRequest) (*emptypb.Empty, error) {
 	log.Println("send message request", req)
 	return nil, nil
